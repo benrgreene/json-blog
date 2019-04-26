948b513dcd4f659be3a28d03be3f73d5a776036e
Ben Greene <ben.greene@bvaccel.com>
2019-04-26 09:26:10 -0700

    chore: start adding in pagination

--- index.js
+++ index.js
@@ -8,12 +8,24 @@ const addListeners = (blogSettings) => {
   window.addEventListener('resize', () => {
     const numToDisplay    = Helpers.maxNumberToDisplay(blogSettings.breakpoints);
     const numberDisplayed = Helpers.numberDisplayed(blogSettings);
-
     // update the articles only if the number to display isn't equal to the number ACTUALLY displayed
     if (numberDisplayed == numToDisplay) { return };
+    // only display if there is no lazyloading OR the articles are in view anyway
+    const shouldDisplay = !blogSettings.lazyload || articlesAreInView(blogSettings);
+    shouldDisplay && Articles.displayArticles(numToDisplay, blogSettings);
+    shouldDisplay && Pagination.displayPagination(numToDisplay, blogSettings);
+  });
 
-    Articles.displayArticles(numToDisplay, blogSettings);
-    Pagination.displayPagination(numToDisplay, blogSettings);
+  // If there's lazyloading, then we'll add a scroll listener for adding
+  // the articles into existence.
+  blogSettings.lazyload && document.addEventListener('scroll', (event) => {
+    const articlesLoaded = articlesAlreadyLoaded(blogSettings);
+    const inView         = articlesAreInView(blogSettings);
+    // If the articles aren't already loaded AND are in view, load them in
+    if (!articlesLoaded && inView) {
+      Articles.displayArticles(numToDisplay, blogSettings);
+      Pagination.displayPagination(numToDisplay, blogSettings);
+    }
   });
 }
 
@@ -24,7 +36,9 @@ export default {
     addListeners(blogSettings);
     // display the initial articles
     const numToDisplay = Helpers.maxNumberToDisplay(blogSettings.breakpoints);
-    Articles.displayArticles(numToDisplay, blogSettings);
-    Pagination.displayPagination(numToDisplay, blogSettings);
+    // only display if there is no lazyloading OR the articles are in view anyway
+    const shouldDisplay = !blogSettings.lazyload || articlesAreInView(blogSettings);
+    shouldDisplay && Articles.displayArticles(numToDisplay, blogSettings);
+    shouldDisplay && Pagination.displayPagination(numToDisplay, blogSettings);
   }
 }
